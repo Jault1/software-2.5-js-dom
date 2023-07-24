7c177a12dbb64221e2761747a706e8e2c4985695
John Ault <john.ault.dev@gmail.com>
2023-07-24 11:11:57 -0400

    Added style stuff

--- js/script.js
+++ js/script.js
@@ -33,3 +33,55 @@ function calcIt(mathFunction) {
 function displayResult(result) {
     document.getElementById("result").innerHTML = result;
 }
+
+function changeStyle() {
+    document.getElementById("result").style.border = '1px solid #fed9b7';
+;    document.getElementById("numLabel").style.color = '#0081a7';
+    document.getElementById("numLabel2").style.color = '#0081a7';
+    document.getElementById("calcHead").classList.add('glow');
+    document.getElementById("resultHead").classList.add('glow');
+
+
+
+
+
+    // body {
+    //     margin: 0;
+    //     font-size: 20px;
+    //     /* font-family: 'Trebuchet MS', Arial, Tahoma, Verdana, Helvetica, sans-serif; */
+    //     font-family: 'Audiowide', cursive;
+    //     /* font-family: 'Hanken Grotesk', sans-serif; */
+    //     color: #00afb9;
+    //     Padding-left: 50px;
+    // }
+    
+    // h1 {
+    //     padding-top: 50px;
+    //     padding-bottom: 6px;
+    //     color: #0081a7;
+    
+    // }
+    
+    // input {
+    //     margin-bottom: 22px;
+    //     margin-left: 22px;
+    // }
+    
+    // label {
+    //     padding-bottom: 22px;
+    //     color: #f07167;
+    // }
+    
+    // h2 {
+    //     padding-top: 50px;
+    //     padding-bottom: 12px;
+    //     margin-left: 50px;
+    // }
+    
+    // #result {
+    //     margin-left: 50px;
+    //     border: 1px solid #f07167;
+    //     width: 10%;
+    //     padding: 12px;
+    // }
+}
